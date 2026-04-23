590039eb6aae745897d44e8882d10ca5b428ff98
Aayush Acharya <aayushacharya007@gmail.com>
2026-04-22 22:58:37 -0500

    Clear button works

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,3 +12,15 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_btnClearCommands_clicked()
+{
+    ui->textDisplayCommands->clear();
+}
+
+
+void MainWindow::on_btnCreateCommand_clicked()
+{
+
+}
+
--- mainwindow.h
+++ mainwindow.h
@@ -17,6 +17,10 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_btnClearCommands_clicked();                 // clears the commands from the slurm commands output area
+    void on_btnCreateCommand_clicked();                 // creates a slurm command based on the input parameters
+
 private:
     Ui::MainWindow *ui;
 };
