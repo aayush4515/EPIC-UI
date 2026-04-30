5f687aa45eb7e546a03a9a4d7570304231b2d3d6
Aayush Acharya <aayushacharya007@gmail.com>
2026-04-30 15:59:02 -0500

    tried fixing built_installer.sh script

--- Installer/build_installer.sh
+++ Installer/build_installer.sh
@@ -6,8 +6,11 @@ set -e
 # go to the folder where the script is located
 cd "$(dirname "$0")"
 
+# create a data directory in Installer/packages/com.vendor/product
+mkdir packages/com.vendor.product/data
+
 # copy the EPIC_UI executable file from build folder to com.vendor.product/data
-cp ../build/Desktop_Qt_6_10_1-Debug/EPIC_UI packages/com.vendor.product/data
+cp ../build/EPIC_UI packages/com.vendor.product/data
 
 # build the EPIC installer
 bin/binarycreator --offline-only -c config/config.xml -p packages EPIC_Installer
